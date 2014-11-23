b4a944f37afb7ea86798714606e6d4c8f48fe05b
nora <bla@blub.de>
2014-11-23 16:29:02 +0100

    parser: first impl and first tests

--- src/impl/java/de/haw/parser/ParserImpl.java
+++ src/impl/java/de/haw/parser/ParserImpl.java
@@ -1,13 +1,176 @@
 package de.haw.parser;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Map.Entry;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * Created by Fenja on 19.11.2014.
- */
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.IndexedWord;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphFactory;
+import edu.stanford.nlp.semgraph.semgrex.SemgrexMatcher;
+import edu.stanford.nlp.semgraph.semgrex.SemgrexPattern;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.util.ArrayCoreMap;
+import edu.stanford.nlp.util.CoreMap;
+
 public class ParserImpl implements Parser{
-    @Override
-    public JSONObject parse(String naturalLanguage) {
-        return null;
-    }
+
+	public ParserImpl(){
+		fillMaps();
+	}
+
+	//THIS IS JUST FOR GETTING STARTED...
+	//possible values for json:
+	String interests="interests";
+	String location="location";
+	String name ="name";
+	String age="age";
+
+	HashMap<String, String> verb_to_key = new HashMap<String, String>();
+
+	private void fillMaps(){
+		verb_to_key.put("like", interests);
+		verb_to_key.put("have", interests);
+		verb_to_key.put("lives", location);
+		verb_to_key.put("name", name);
+		verb_to_key.put("call", name);
+	}
+
+
+	@Override
+	public JSONObject parse(String naturalLanguage) {
+
+
+		//create parsing toolchain
+		Properties props=new Properties();
+		props.put("annotators","tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		Annotation annotation = new Annotation(naturalLanguage);
+		pipeline.annotate(annotation);
+
+
+		List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+		ArrayCoreMap sentence = (ArrayCoreMap) sentences.get(0);
+		Map<IndexedWord, List<IndexedWord>> verbs= get_full_verb_and_object(sentence);
+		JSONObject json=keywordlist_to_json(verbs);
+
+
+		return json;
+	}
+
+
+	private JSONObject keywordlist_to_json(Map <IndexedWord, List<IndexedWord>> keywords){
+
+		Map<String,List<String>> prevJson=new HashMap<String,List<String>>();
+
+		for(Map.Entry<IndexedWord, List<IndexedWord>> entry:keywords.entrySet()){
+			String key=verb_to_key.get((entry.getKey()).lemma());
+			// System.out.println("trying to find key: "+entry.getKey()+" with value "+ entry.getValue());
+			if(key != null){
+				List<String> val = prevJson.get(key);
+				List<String> val2=words_to_lemmas(entry.getValue());
+				//		System.out.println("lemmas: "+val2);
+				if(val != null)val2.addAll(val);
+				prevJson.put(key, val2);
+			}//TODO: ELSE: resparse for object
+
+		}
+
+		// 	    System.out.println("created map:"+prevJson);
+		//create JSON Object
+		JSONObject outerJson=new JSONObject();
+
+		if(prevJson.size() > 1){
+			ArrayList<JSONObject> jsons=new ArrayList<JSONObject>();
+			for (Entry<String,List<String>> entry: prevJson.entrySet()){
+				jsons.add(keywords_to_json2D(entry));
+			}
+			try {
+				outerJson.put("and",jsons);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(prevJson.size()==1){
+			outerJson=keywords_to_json2D(prevJson.entrySet().iterator().next());
+		}
+		return outerJson;
+
+	}
+
+	private JSONObject keywords_to_json2D(Map.Entry<String,List<String>> entry){
+
+		JSONObject json=new JSONObject();
+		// 	   for (Entry<String,List<String>> entry: prevJson.entrySet()){
+		try {
+			json.put("weight", 1);
+			json.put("attribute", entry.getKey());
+			json.put("value_type", "string_list");
+			JSONArray list=new JSONArray();
+			list.put(entry.getValue());
+			//	list.addAll(entry.getValue());
+			json.put("value", entry.getValue());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return json;
+	}
+
+
+
+
+	private List<String> words_to_lemmas(List<IndexedWord> list){
+		List<String> lemmas=new ArrayList<String>();
+		for(int i=0; i<list.size(); i++) lemmas.add(list.get(i).lemma());
+		return lemmas;
+	}
+
+
+	/*
+	 * returns main verbs with corresponding object
+	 * */
+	private Map<IndexedWord, List<IndexedWord>> get_full_verb_and_object(ArrayCoreMap annot_sentence){
+
+		Tree tree = annot_sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+		SemanticGraph graph = SemanticGraphFactory.generateUncollapsedDependencies(tree);
+		System.err.println(graph); 
+
+		//		SemgrexPattern semgrex = SemgrexPattern.compile("{tag:/NN.*/}=A [<<dobj {tag:/VB.*/}=B |<<pobj {}=C << {tag:/VB.*/}=B]  ");
+		SemgrexPattern semgrex = SemgrexPattern.compile("{tag:/NN.*/}=A <<dobj {tag:/VB.*/}=B ");
+		SemgrexMatcher matcher = semgrex.matcher(graph);
+		Map<IndexedWord, List<IndexedWord>> verbs=new HashMap<IndexedWord, List<IndexedWord>>();
+		while (matcher.find()) {
+			// System.err.println(matcher.getNode("A") + " <<dobj " + matcher.getNode("B"));
+			IndexedWord nodeA = matcher.getNode("A");
+			IndexedWord nodeB = matcher.getNode("B");
+			//			System.out.println("Node B:"+nodeB);
+			//			System.out.println("Verbalphrase:" + nodeA + " Lemma:"+nodeA.lemma() + " tag:"+nodeA.tag());
+			List<IndexedWord>temp=verbs.get(nodeB);
+			if(temp!=null){
+				temp.add(nodeA);
+			}else{
+				temp=new ArrayList<IndexedWord>();
+				temp.add(nodeA);
+			}
+			verbs.put(nodeB,temp);
+		}
+
+		//	System.out.println("verbs: "+verbs);
+
+		return verbs;
+	}
+
 }
--- src/test/java/de/haw/parser/ParserTest.java
+++ src/test/java/de/haw/parser/ParserTest.java
@@ -1,20 +1,73 @@
 package de.haw.parser;
 
+import static org.junit.Assert.assertTrue;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Created by Fenja on 19.11.2014.
- */
+
 public class ParserTest {
 
-    @Before
-    public void setUp(){
-        //if needed
-    }
+	Parser parser;
+	@Before
+	public void setUp() {
+		System.out.println("Starting Parser Test");
+		parser=new ParserImpl();
+	}
 
-    @Test
-    public void parseTest(){
-        //Todo
-    }
+
+	@Test
+	public void testSimpleSentences() throws JSONException {
+		JSONObject test01=parser.parse("who likes dogs?");
+		System.out.println("json:"+test01.toString());
+		assertTrue(test01.getString("attribute").equals("interests"));
+		assertTrue(test01.getJSONArray("value").getString(0).equals("dog"));
+
+
+		JSONObject test02=parser.parse("who likes cats and dogs?");
+		System.out.println("json:"+test02.toString());
+		assertTrue(test02.getString("attribute").equals("interests"));
+		assertTrue(test02.getJSONArray("value").getString(0).equals("cat"));
+		assertTrue(test02.getJSONArray("value").getString(1).equals("dog"));
+
+		JSONObject test03=parser.parse("who likes (dogs and cats) or pigs?");
+		System.out.println("json:"+test03.toString());
+		assertTrue(test03.getString("attribute").equals("interests"));
+		assertTrue(test03.getJSONArray("value").getString(0).equals("dog"));
+		assertTrue(test03.getJSONArray("value").getString(1).equals("pig"));
+		assertTrue(test03.getJSONArray("value").getString(2).equals("cat"));
+
+
+		JSONObject test04=parser.parse("who is called Jane?");
+		System.out.println("json:"+test04.toString());
+		assertTrue(test04.getString("attribute").equals("name"));
+		assertTrue(test04.getJSONArray("value").getString(0).equals("Jane"));
+
+
+
+		/*
+		 * more dimensions:
+		 */
+		JSONObject test05=parser.parse("who is called Jane and likes cats?");
+		System.out.println(test05);
+		JSONArray list05=test05.getJSONArray("and");
+		assertTrue(list05.getJSONObject(0).getString("attribute").equals("name"));		
+		assertTrue(list05.getJSONObject(0).getJSONArray("value").get(0).equals("Jane"));
+		assertTrue(list05.getJSONObject(1).getString("attribute").equals("interests"));		
+		assertTrue(list05.getJSONObject(1).getJSONArray("value").get(0).equals("cat"));
+
+	}
+
+	@Ignore
+	@Test
+	public void testMoreComplexSentences() {
+		//fail("Not yet implemented");
+		//assertTrue
+	}
+
 }
