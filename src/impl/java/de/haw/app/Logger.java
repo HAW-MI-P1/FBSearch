7d6f0e704404ce07ba79f4a4fb819412b3d56e3b
SchneiderGitHub <Hans.Mander@outlook.de>
2014-12-14 19:00:07 +0100

    Added Image Detector stub. Added log to file feature. Added some tests for controller.

--- src/impl/java/de/haw/app/Logger.java
+++ src/impl/java/de/haw/app/Logger.java
@@ -23,6 +23,14 @@ package de.haw.app;
  *                                 Imports                                    *
  *****************************************************************************/
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import org.joda.time.DateTime;
+
 import de.haw.model.ComponentID;
 
 /******************************************************************************
@@ -31,6 +39,12 @@ import de.haw.model.ComponentID;
 
 public class Logger
 {
+
+/******************************************************************************
+ *                                  Fields                                    *
+ *****************************************************************************/
+
+	private static BufferedWriter writer;
 	
 /******************************************************************************
  *                              Public Methods                                *
@@ -38,9 +52,46 @@ public class Logger
 	
     public static void log(String msg, ComponentID component)
     {
-        System.out.println(component.toString() + ": " + msg);
+    	String now = DateTime.now().toString("yyyy_MM_dd_HH_mm_ss");
+    	String str = ": " + component.toString() + ": " + msg;
+    	
+        System.out.println(now + str);
+         
+        if(writer == null)
+        {
+        	 try
+        	 {
+				 writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("FBSearchLog_" + now + ".log"), "utf-8"));
+			 }
+        	 catch (UnsupportedEncodingException e)
+        	 {
+				 e.printStackTrace();
+			 }
+        	 catch (FileNotFoundException e)
+        	 {
+			 	 e.printStackTrace();
+			 }
+        }
         
-        // TODO log to file?
-        // TODO log to gui?
+        try
+        {
+			writer.write(now + str);
+	        writer.flush();
+		}
+        catch (IOException e)
+        {
+			e.printStackTrace();
+		}
+    }
+    
+    @Override
+    protected void finalize() throws Throwable
+    {
+    	if(writer != null)
+    	{
+    		writer.close();
+    	}
+    	
+		super.finalize();
     }
 }
--- src/test/java/de/haw/controller/ControllerTests.java
+++ src/test/java/de/haw/controller/ControllerTests.java
@@ -5,5 +5,5 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({/*Operation1Test.class,*/})
+@SuiteClasses({ExceptionTest.class})
 public class ControllerTests {}
