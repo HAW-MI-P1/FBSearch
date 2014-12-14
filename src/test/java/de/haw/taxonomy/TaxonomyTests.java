fcc14480612c35dfb0780f1c98d3f59cc47be6a5
Neonchen <no@reply.com>
2014-12-14 21:21:48 +0100

    welcome the taxonomy class

--- src/test/java/de/haw/taxonomy/TaxonomyTests.java
+++ src/test/java/de/haw/taxonomy/TaxonomyTests.java
@@ -1,11 +1,26 @@
 package de.haw.taxonomy;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TaxonomyTests {
 
-	@Test
-	public void testSearch()	{
+	Taxonomy taxonomy;
+	
+	@Before
+	public void setup(){
+		taxonomy = new TaxonomyImpl();
 	}
-
+	
+	@Test
+	public void testSearch(){
+		List<String> expResult = Arrays.asList("Deutschland", "Altona");
+		List<String> result = taxonomy.search("Hamburg");
+		assertEquals(result,expResult);
+	}
 }
