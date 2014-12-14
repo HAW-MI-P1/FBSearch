9dd399df0e08e8c593b26db327923e593950c835
Fenja <fenja.harbke@haw-hamburg.de>
2014-12-14 14:06:15 +0100

    insert simple collection as MockUpDB

--- src/impl/java/de/haw/db/MockUpDBImpl.java
+++ src/impl/java/de/haw/db/MockUpDBImpl.java
@@ -3,19 +3,26 @@ package de.haw.db;
 import de.haw.model.types.Type;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
  * Created by Fenja on 13.12.2014.
  */
 public class MockUpDBImpl implements DB {
+
+    private Collection<Type> db;
+
+    public MockUpDBImpl() {
+        this.db = new ArrayList<Type>();
+    }
     @Override
     public void save(int searchID, String naturalLanguage, JSONObject requests, Collection<Type> result) {
-
+        this.db = result;
     }
 
     @Override
     public Collection<Type> load(int parentSearchID) {
-        return null;
+        return this.db;
     }
 }
