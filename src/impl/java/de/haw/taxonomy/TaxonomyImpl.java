338a1dea5be173f938f35b482081cc1278c115e7
Neonchen <no@reply.com>
2014-12-14 20:16:05 +0100

    taxonomy init

---
+++ src/impl/java/de/haw/taxonomy/TaxonomyImpl.java
@@ -0,0 +1,14 @@
+package de.haw.taxonomy;
+
+import java.util.List;
+
+public class TaxonomyImpl implements Taxonomy{
+	
+	public TaxonomyImpl(){}
+	
+	@Override
+	public List<String> search(String item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/test/java/de/haw/taxonomy/TaxonomyTests.java
@@ -0,0 +1,11 @@
+package de.haw.taxonomy;
+
+import org.junit.Test;
+
+public class TaxonomyTests {
+
+	@Test
+	public void testSearch()	{
+	}
+
+}
