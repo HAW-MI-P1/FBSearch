b55a8f4e0976190b1ff057e7fd64b23eeb4d0bc9
matthies <matthies.becker@gmx.de>
2015-01-12 15:45:21 -0200

    TESTING: Enhanced taxonomy tests

--- src/test/java/de/haw/taxonomy/TaxonomyTests.java
+++ src/test/java/de/haw/taxonomy/TaxonomyTests.java
@@ -2,7 +2,9 @@ package de.haw.taxonomy;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -21,6 +23,43 @@ public class TaxonomyTests {
 	public void testSearch(){
 		List<String> expResult = Arrays.asList("Deutschland", "Altona");
 		List<String> result = taxonomy.search("place","Hamburg");
-		assertEquals(result,expResult);
-	}
+		assertEquals(expResult, result);
+
+        expResult = Arrays.asList("Hamburg");
+        result = taxonomy.search("place","Altona");
+        assertEquals(expResult, result);
+
+        expResult = new ArrayList<String>();
+        result = taxonomy.search(null, "Altona");
+        assertEquals(expResult, result);
+
+        expResult = new ArrayList<String>();
+        result = taxonomy.search("place", null);
+        assertEquals(expResult, result);
+
+        expResult = new ArrayList<String>();
+        result = taxonomy.search(null, null);
+        assertEquals(expResult, result);
+
+        expResult = new ArrayList<String>();
+        result = taxonomy.search("", "Altona");
+        assertEquals(expResult, result);
+
+        expResult = new ArrayList<String>();
+        result = taxonomy.search("place", "");
+        assertEquals(expResult, result);
+
+        expResult = new ArrayList<String>();
+        result = taxonomy.search("", "");
+        assertEquals(expResult, result);
+
+        expResult = new ArrayList<String>();
+        result = taxonomy.search("place", "a");
+        assertEquals(expResult, result);
+    }
+
+    @Test(expected = org.apache.jena.riot.RiotNotFoundException.class)
+    public void fileMissing() throws org.apache.jena.riot.RiotNotFoundException {
+        taxonomy = new TaxonomyImpl(Arrays.asList("fasdf"));
+    }
 }
