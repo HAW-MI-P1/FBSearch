3a203461148332a152ff7516b041d36aebffd7c0
matthies <matthies.becker@gmx.de>
2015-01-13 14:32:54 -0200

    TESTING: Enhanced fuzzy tests, removed duplicated fuzzy test class

--- src/test/java/de/haw/fuzzy/FuzzyTests.java
+++ src/test/java/de/haw/fuzzy/FuzzyTests.java
@@ -1,9 +1,59 @@
 package de.haw.fuzzy;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-@RunWith(Suite.class)
-@SuiteClasses({GetSynonymTest.class})
-public class FuzzyTests {}
+import java.util.Collection;
+
+import static org.junit.Assert.fail;
+
+public class FuzzyTests {
+
+    Fuzzy fuzzy;
+
+    @Before
+    public void setUp() throws Exception {
+        this.fuzzy = new FuzzyImpl();
+    }
+
+    @Test
+    public void testGetSynonym() {
+        Collection<String> response = this.fuzzy.getSynonym("lion", 0x0F);
+
+        if(response.size() <= 1){ fail(); }
+    }
+
+    @Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+    public void testGetSynonymWordNull() {
+        this.fuzzy.getSynonym(null, 0x0F);
+    }
+
+    @Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+    public void testGetSynonymWordEmpty() {
+        this.fuzzy.getSynonym("", 0x0F);
+    }
+
+    @Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+    public void testGetSynonymOptZero() {
+        this.fuzzy.getSynonym("boar", 0x00);
+    }
+
+    @Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+    public void testGetSynonymOptInvalid() {
+        this.fuzzy.getSynonym("boar", -1);
+    }
+
+    @Test
+    public void testGetAllSynonymsForTypo() {
+        Collection<String> response = fuzzy.getSynonym("dfsgdfgdf", 0x0F);
+        Assert.assertEquals(0, response.size());
+    }
+
+    @Test
+    public void testGetAllSynonymsForLion() {
+        Collection<String> synonyms = fuzzy.getSynonym("lion", 0x01);
+        Assert.assertEquals(13, synonyms.size());
+    }
+
+}
