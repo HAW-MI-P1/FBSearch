3b58aa1883ad6512272303c1bb2b79092da2bc11
vkagadij <vkagadij@MC00023110.one.steria.dom>
2015-01-17 18:40:40 +0100

    Wrapper, Controller Tests

--- src/test/java/de/haw/controller/ControllerTests.java
+++ src/test/java/de/haw/controller/ControllerTests.java
@@ -1,9 +1,106 @@
 package de.haw.controller;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({ExceptionTest.class})
-public class ControllerTests {}
+import de.haw.db.DB;
+import de.haw.db.DBImpl;
+import de.haw.detector.Detector;
+import de.haw.detector.DetectorImpl;
+import de.haw.fuzzy.Fuzzy;
+import de.haw.fuzzy.FuzzyImpl;
+import de.haw.model.types.Type;
+import de.haw.parser.Parser;
+import de.haw.parser.ParserImpl;
+import de.haw.taxonomy.Taxonomy;
+import de.haw.taxonomy.TaxonomyImpl;
+import de.haw.wrapper.Wrapper;
+import de.haw.wrapper.WrapperImpl;
+
+public class ControllerTests {
+
+	Controller controller;
+
+	Parser parser;
+	Wrapper wrapper;
+	DB db;
+	Detector detector;
+	Taxonomy taxonomy;
+	Fuzzy fuzzy;
+
+	@Before
+	public void setUp() {
+		System.out.println("Starting Controller Test");
+		parser = new ParserImpl();
+		wrapper = new WrapperImpl();
+		db = new DBImpl();
+		detector = new DetectorImpl();
+		taxonomy = new TaxonomyImpl(Arrays.asList("place"));
+		fuzzy = new FuzzyImpl();
+		controller = new ControllerImpl(parser, wrapper, db, detector,
+				taxonomy, fuzzy);
+
+	}
+
+	@Test
+	public void TestSearchEmpty() {
+		Collection<Type> resultData = new ArrayList<Type>();
+		Collection<Type> result = controller.search(0, "");
+		assertEquals(result, resultData);
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestSearchNull() {
+		int i = new Integer(null);
+		Collection<Type> result = controller.search(i, null);
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestSearch01() {
+		int i = -1;
+		Collection<Type> result = controller.search(i, "sd");
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestSearchExtendedNull() {
+		int i = new Integer(null);
+		int j = new Integer(null);
+		Collection<Type> resultData = new ArrayList<Type>();
+		Collection<Type> result = controller.searchExtended(i, j, "", false);
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestSearchExtended01() {
+		int i = -1;
+		int j = -1;
+		Collection<Type> result = controller.searchExtended(i, j, "sd", true);
+	}
+
+	@Test
+	public void TestSearchRecsEmpty() {
+		Collection<String> resultData = new ArrayList<String>();
+		Collection<String> result = controller.searchRecs("");
+		assertEquals(result, resultData);
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestSearchRecsNull() {
+		Collection<String> result = controller.searchRecs(null);
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestSearchRecs01() {
+		Collection<String> result = controller.searchRecs("sadfdsd asdf");
+	}
+
+}
--- src/test/java/de/haw/wrapper/WrapperTests.java
+++ src/test/java/de/haw/wrapper/WrapperTests.java
@@ -1,9 +1,82 @@
 package de.haw.wrapper;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import static org.junit.Assert.*;
 
-@RunWith(Suite.class)
-@SuiteClasses({/*Operation1Test.class,*/})
-public class WrapperTests {}
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.haw.model.WebPicture;
+import de.haw.model.types.Type;
+
+public class WrapperTests {
+
+	Wrapper wrapper;
+
+	@Before
+	public void setUp() {
+		System.out.println("Starting Wrapper Test");
+		wrapper = new WrapperImpl();
+
+	}
+
+	@Test
+	public void TestEmpty01() {
+		List<String> test = new ArrayList<String>();
+		Collection<Type> resultData = new ArrayList<Type>();
+		Collection<Type> result = wrapper.searchForName("", test);
+		assertEquals(result, resultData);
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestNull01() {
+		Collection<Type> result = wrapper.searchForName(null, null);
+	}
+
+	@Test
+	public void TestEmpty02() {
+		Collection<Type> test = new ArrayList<Type>();
+		List<WebPicture> res = new ArrayList<WebPicture>();
+		List<WebPicture> result = wrapper.getPicturesForPersons(test);
+		assertEquals(result, res);
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestNull02() {
+		List<WebPicture> result = wrapper.getPicturesForPersons(null);
+	}
+
+	@Test
+	public void TestEmpty03() {
+		assertFalse(wrapper.idLivesIn("", ""));
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestNull03() {
+		assertFalse(wrapper.idLivesIn(null, null));
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void Test01() {
+		wrapper.idLivesIn("123 sdf", "Kujh");
+	}
+
+	@Test
+	public void TestEmpty04() {
+		JSONObject test = new JSONObject();
+		Collection<Type> test2 = new ArrayList<Type>();
+		Collection<Type> res = new ArrayList<Type>();
+		Collection<Type> result = wrapper.collectExtended(test, test2);
+		assertEquals(result, res);
+	}
+
+	@Test(expected = de.haw.model.exception.IllegalArgumentException.class)
+	public void TestNull04() {
+		Collection<Type> result = wrapper.collectExtended(null, null);
+	}
+
+}
